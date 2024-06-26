3df03266446d90f722b73415a3dc4cd9b1f6791a
Pitza <luishc23@icloud.com>
2024-06-26 17:19:55 -0600

    Create exercise 2.10 and 2.9 Python scripts. Introduce them to the user's favorite number and basic arithmetic operations.

---
+++ Part1/exercise 2.10/main.py
@@ -0,0 +1,11 @@
+# Exercise 2.10 Favorite Number: Store your favorite number in a variable. Then, using that variable,
+# create a message that reveals your favorite number. Print that message.
+
+def main():
+    favorite_number = 23
+    message = f"My favorite number is {favorite_number}."
+    print(message)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Part1/exercise 2.9/main.py
@@ -0,0 +1,12 @@
+# Exercise 2.9 Number Eight:
+# Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+
+def main():
+    print(f'Addition 5 + 3 = {5 + 3}')  # Addition
+    print(f'Subtraction 10 - 2 = {10 - 2}')  # Subtraction
+    print(f'Multiplication 4 * 2 = {4 * 2}')  # Multiplication
+    print(f'Division 16 / 2 = {16 / 2}')  # Division
+
+
+if __name__ == "__main__":
+    main()
