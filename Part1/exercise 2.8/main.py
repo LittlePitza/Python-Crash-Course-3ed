7c906a0e8d8f6cd6772881f69b08cb06d42a318c
Pitza <luishc23@icloud.com>
2024-06-26 11:10:31 -0600

    Remove file suffix and add comments for better code readability.

--- Part1/exercise 2.8/main.py
+++ Part1/exercise 2.8/main.py
@@ -1,9 +1,9 @@
 # Exercise 2.8 File Extensions.
 
 def main(filename):
-    new_file = filename.removesuffix(".txt")
-    print(f"Filename: {filename} \nNew filename: {new_file}")
+    new_file = filename.removesuffix(".txt")  # Removes the suffix ".txt" from the filename
+    print(f"Filename: {filename} \nNew filename: {new_file}")  # Output: Filename: hello.txt New filename: hello
 
 
 if __name__ == "__main__":
-    main("hello.txt")
+    main("hello.txt")  # Function call with the filename "hello.txt" as an argument
