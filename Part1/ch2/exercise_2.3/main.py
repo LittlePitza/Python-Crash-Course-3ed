14740415fe4fe489e2fe676fc2ff90a6f6712cd2
Luis Eduardo Hernandez Cruz <luishc888@gmail.com>
2024-06-26 10:16:52 -0600

    terminate

---
+++ Part1/ch2/exercise_2.3/main.py
@@ -0,0 +1,11 @@
+# exercise 2.3 Personal Message
+
+
+def main():
+    user_name = input("Enter your name: ")
+    print(f"Hello {user_name}, would you like to learn some Python today?")
+
+
+if __name__ == "__main__":
+    main()
+
