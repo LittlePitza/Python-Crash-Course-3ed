6e670710edef876c03585684cc52d852ab021796
Pitza <luishc23@icloud.com>
2024-06-28 00:09:04 -0600

    Add Exercise 4.13 Buffet code and demonstrate tuple immutability.

---
+++ Part1/exercise 4.13/main.py
@@ -0,0 +1,17 @@
+# Exercise 4.13 Buffet
+
+def main():
+    menu = ('pizza', 'hamburger', 'hotdog', 'pasta', 'salad')
+    print('Welcome to the buffet!\nPick your favorite food from the menu below and enjoy your evening!\n')
+    for food in menu:
+        print(f'Plate {menu.index(food)+1}: {food}')
+
+    # menu[0] = 'sushi' # This line will raise an error because tuples are immutable
+    menu = ('sushi', 'hamburger', 'hotdog', 'pasta', 'salad')
+    print('\nOur menu has been updated!\n')
+    for food in menu:
+        print(f'Plate {menu.index(food)+1}: {food}')
+
+
+if __name__ == "__main__":
+    main()
